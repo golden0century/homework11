f04eb9afe58583a758c067b7a11949d058c11ad6
golden0century <67648604+golden0century@users.noreply.github.com>
2020-11-17 11:43:01 +0300

    make changes after review

--- helpers.py
+++ helpers.py
@@ -1,2 +1,5 @@
 CHROME_DRIVER_PATH = '/Users/v.konyashina/автоматизация/chromedriver'
 BASE_URL = 'https://www.cian.ru/'
+
+def update_locator(locator, text):
+    return locator.format(text)
--- pages/price.py
+++ pages/price.py
@@ -1,58 +1,53 @@
 from pages.page import BasePage
 from pages.region_value import RegionValueClass
-
+from helpers import update_locator
 
 class PricePageLocators:
-    Price_Page_Header = "//div[contains(@class,'cf_header--embedded')]"
-    Price_Page_Banner = "//div[@class='banner-top']"
-    Price_Page_Banner_Capital = "//img[contains(@src, 'subscriptions-banner')]"
-    Price_Page_Banner_Region = "//img[contains(@src, 'free-in-regions-banner')]"
-    Price_Page_MainBlock = "//div[@class='price-page']"
-    Price_Page_ChooseRegionBlock = "//select[@id ='body_ddlRegions']"
-    Price_Page_ChooseRegionItem = "//option[@value ='{}']"
-    Price_Page_ChooseRegionItemSelected = "//option[@value ='{}'][@selected='selected']/ancestor::*[@class='dmSelect']"
-    Price_Page_SubscriptionBlock = "//a[@href='https://promo.cian.ru/ciansubscriptions']/ancestor::*[@class='c_mt15']"
-    Price_Page_PricesBlock = "//table[@class = 'publishPrices']"
-    Price_Page_TarifBlock = "//table[@id = 'tarif']"
-    Price_Page_Footer = "//div[@class = 'cf-footer']"
-
-    @staticmethod
-    def update_locator(locator, text):
-        return locator.format(text)
-
+    PRICE_PAGE_HEADER = "//div[contains(@class,'cf_header--embedded')]"
+    PRICE_PAGE_BANNER = "//div[@class='banner-top']"
+    PRICE_PAGE_BANNER_CAPITAL = "//img[contains(@src, 'subscriptions-banner')]"
+    PRICE_PAGE_BANNER_REGION = "//img[contains(@src, 'free-in-regions-banner')]"
+    PRICE_PAGE_MAIN_BLOCK = "//div[@class='price-page']"
+    PRICE_PAGE_CHOOSE_REGION_BLOCK= "//select[@id ='body_ddlRegions']"
+    PRICE_PAGE_CHOOSE_REGION_ITEM = "//option[@value ='{}']"
+    PRICE_PAGE_CHOOSE_REGION_ITEM_SELECTED = "//option[@value ='{}'][@selected='selected']/ancestor::*[@class='dmSelect']"
+    PRICE_PAGE_SUBSCRIPTION_BLOCK = "//a[@href='https://promo.cian.ru/ciansubscriptions']/ancestor::*[@class='c_mt15']"
+    PRICE_PAGE_PRICE_BLOCK = "//table[@class = 'publishPrices']"
+    PRICE_PAGE_TARIFF_BLOCK = "//table[@id = 'tarif']"
+    PRICE_PAGE_FOOTER = "//div[@class = 'cf-footer']"
 
 class PricePageActions(BasePage):
 
     def choose_region(self, poligon_id):
-        locator_Moscow = PricePageLocators.update_locator(PricePageLocators.Price_Page_ChooseRegionItemSelected,
+        locator_Moscow = update_locator(PricePageLocators.PRICE_PAGE_CHOOSE_REGION_ITEM_SELECTED,
                                                           RegionValueClass.moscow)
-        locator_poligon = PricePageLocators.update_locator(PricePageLocators.Price_Page_ChooseRegionItem, poligon_id)
-        locator_poligon_selected = PricePageLocators.update_locator(
-            PricePageLocators.Price_Page_ChooseRegionItemSelected, poligon_id)
-        self.find_element(PricePageLocators.Price_Page_MainBlock)
-        self.find_element(PricePageLocators.Price_Page_ChooseRegionBlock)
+        locator_poligon = update_locator(PricePageLocators.PRICE_PAGE_CHOOSE_REGION_ITEM, poligon_id)
+        locator_poligon_selected = update_locator(
+            PricePageLocators.PRICE_PAGE_CHOOSE_REGION_ITEM_SELECTED, poligon_id)
+        self.find_element(PricePageLocators.PRICE_PAGE_MAIN_BLOCK)
+        self.find_element(PricePageLocators.PRICE_PAGE_CHOOSE_REGION_BLOCK)
         self.click_to_element(locator_Moscow)
         self.click_to_element(locator_poligon)
         self.find_element(locator_poligon_selected)
         assert (self.driver.current_url, self.base_url + "price/?polygonId=" + poligon_id)
 
     def check_capital_price(self, poligon_id):
-        locator_poligon = PricePageLocators.update_locator(PricePageLocators.Price_Page_ChooseRegionItemSelected,
+        locator_poligon = update_locator(PricePageLocators.PRICE_PAGE_CHOOSE_REGION_ITEM_SELECTED,
                                                            poligon_id)
-        self.find_element(PricePageLocators.Price_Page_MainBlock)
-        self.find_element(PricePageLocators.Price_Page_ChooseRegionBlock)
+        self.find_element(PricePageLocators.PRICE_PAGE_MAIN_BLOCK)
+        self.find_element(PricePageLocators.PRICE_PAGE_CHOOSE_REGION_BLOCK)
         self.find_element(locator_poligon)
-        self.find_element(PricePageLocators.Price_Page_SubscriptionBlock)
-        self.find_element(PricePageLocators.Price_Page_Footer)
+        self.find_element(PricePageLocators.PRICE_PAGE_SUBSCRIPTION_BLOCK)
+        self.find_element(PricePageLocators.PRICE_PAGE_FOOTER)
 
     def check_region_price(self, poligon_id):
-        locator_poligon = PricePageLocators.update_locator(PricePageLocators.Price_Page_ChooseRegionItemSelected,
+        locator_poligon = update_locator(PricePageLocators.PRICE_PAGE_CHOOSE_REGION_ITEM_SELECTED,
                                                            poligon_id)
-        self.find_element(PricePageLocators.Price_Page_Banner)
-        self.find_element(PricePageLocators.Price_Page_Banner_Region)
-        self.find_element(PricePageLocators.Price_Page_MainBlock)
-        self.find_element(PricePageLocators.Price_Page_ChooseRegionBlock)
+        self.find_element(PricePageLocators.PRICE_PAGE_BANNER)
+        self.find_element(PricePageLocators.PRICE_PAGE_BANNER_REGION)
+        self.find_element(PricePageLocators.PRICE_PAGE_MAIN_BLOCK)
+        self.find_element(PricePageLocators.PRICE_PAGE_CHOOSE_REGION_BLOCK)
         self.find_element(locator_poligon)
-        self.find_elements(PricePageLocators.Price_Page_PricesBlock)
-        self.find_element(PricePageLocators.Price_Page_TarifBlock)
-        self.find_element(PricePageLocators.Price_Page_Footer)
+        self.find_elements(PricePageLocators.PRICE_PAGE_PRICE_BLOCK)
+        self.find_element(PricePageLocators.PRICE_PAGE_TARIFF_BLOCK)
+        self.find_element(PricePageLocators.PRICE_PAGE_FOOTER)
